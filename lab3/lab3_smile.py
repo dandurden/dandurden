4a1e1118bf250105f9a19a696769f70c16d77bc7
dandurden <dandurden@yandex.ru>
2021-08-26 22:56:12 +0300

    нарисовал и жестко закодировал круги

--- lab3/lab3_smile.py
+++ lab3/lab3_smile.py
@@ -2,18 +2,22 @@ import pygame
 from pygame.draw import *
 
 pygame.init()
+screen = pygame.display.set_mode((400, 400))
 FPS = 30
+color = {'yellow': (242, 245, 66), 'red': (255, 0, 0), 'black': (0, 0, 0)}
+x = y = 0
 
 
-def drow_circle(color, x, y):
+def drow_circle(color, x, y, r):
     """
     рисуем круг в центре экрана
     :param color: задает цвет фона круга
     :param x: координата х
     :param y: координата у
+    :param r: радиус круга
     :return: None
     """
-    pass
+    circle(screen, color, (x, y), r)
 
 
 def drow_line(color, x, y):
@@ -27,15 +31,16 @@ def drow_line(color, x, y):
     pass
 
 
-drow_circle()
-drow_circle()
-drow_circle()
-drow_circle()
-drow_circle()
-drow_line()
-drow_line()
-drow_line()
-
+drow_circle(color['yellow'], 200, 200, 130)
+drow_circle(color['red'], 140, 170, 30)
+drow_circle(color['black'], 140, 170, 13)
+drow_circle(color['red'], 260, 170, 20)
+drow_circle(color['black'], 260, 170, 10)
+# drow_line()
+# drow_line()
+# drow_line()
+pygame.display.update()
+clock = pygame.time.Clock()
 finished = False
 while not finished:
     clock.tick(FPS)
