418919b3011154e75dd8552c7a2d82295710e2bd
dandurden <dandurden@yandex.ru>
2021-08-29 15:59:40 +0300

    нарисовал облака

--- lab3/lab3_pikcher.py
+++ lab3/lab3_pikcher.py
@@ -14,9 +14,34 @@ color = {'yellow': (242, 245, 66),
          'brown': (71, 68, 29),
          'ligth_brown': (163, 138, 70)}
 
-screen.fill((174, 244, 245))  # закрасили фон
-
+# закрасили фон
+screen.fill((174, 244, 245))
+# нарисовали траву
 rect(screen, color['green'], ((0, y/2), (x, y/2)))
+# нарисовал облако
+for i in [0, 20, 40, 60]:
+    if i == 20 or i == 40:
+        circle(screen, color['white'], (x/8+i, y/6), 20)
+        circle(screen, color['white'], (x / 8 + i, y / 6-20), 20)
+    else:
+        circle(screen, color['white'], (x / 8 + i, y / 6), 20)
+# нарисовал облако
+for i in [0, 15, 30, 45]:
+    if i == 15 or i == 30:
+        circle(screen, color['white'], (x/2+i, y/5), 15)
+        circle(screen, color['white'], (x / 2 + i, y / 5 -15), 15)
+    else:
+        circle(screen, color['white'], (x / 2 + i, y / 5), 15)
+# нарисовал облако
+for i in [0, 20, 40, 60]:
+    if i == 20 or i == 40:
+        circle(screen, color['white'], (x/1.2 + i, y/6), 20)
+        circle(screen, color['white'], (x / 1.2 + i, y / 6-20), 20)
+    else:
+        circle(screen, color['white'], (x / 1.2 + i, y / 6), 20)
+# нарисовал солнце
+circle(screen, color['yellow'], (35, 35), 25)
+# рисуем дом
 
 
 
