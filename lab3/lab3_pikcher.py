3327767c9d8274ba28bc9f6bb0d1a7fc3227a09c
dandurden <dandurden@yandex.ru>
2021-08-29 18:58:14 +0300

    сделал функцию которая рисут дома

--- lab3/lab3_pikcher.py
+++ lab3/lab3_pikcher.py
@@ -7,12 +7,13 @@ y = 600  # высота
 screen = pygame.display.set_mode((x, y))
 FPS = 30
 color = {'yellow': (242, 245, 66),
-         'pink': (240, 60, 141),
+         'pink': (224, 61, 36),
          'green': (105, 196, 96),
          'dark_green': (57, 102, 53),
          'white': (255, 255, 255),
-         'brown': (71, 68, 29),
-         'ligth_brown': (163, 138, 70)}
+         'brown': (158, 119, 85),
+         'ligth_brown': (163, 138, 70),
+         'blue': (125, 174, 212)}
 
 # закрасили фон
 screen.fill((174, 244, 245))
@@ -22,26 +23,32 @@ rect(screen, color['green'], ((0, y / 2), (x, y / 2)))
 
 def draw_cloud(x, y, r):
     """
-
+    эта функция рисует облака по заданным координатам и радиусу
     :param x: координата х
     :param y: координата у
     :param r: радиус облачка
     :return:
     """
-    if r > 15:
-        for i in [0, 20, 40, 60]:
-            if i == 20 or i == 40:
-                circle(screen, color['white'], (x + i, y), r)
-                circle(screen, color['white'], (x + i, y - r), r)
-            else:
-                circle(screen, color['white'], (x + i, y), r)
-    else:
-        for i in [0, 15, 30, 45]:
-            if i == 15 or i == 30:
-                circle(screen, color['white'], (x + i, y), r)
-                circle(screen, color['white'], (x + i, y - r), r)
-            else:
-                circle(screen, color['white'], (x + i, y), r)
+    for i in [0 * r, r, 2 * r, 3 * r]:
+        if i == r or i == 2 * r:
+            circle(screen, color['white'], (x + i, y), r)
+            circle(screen, color['white'], (x + i, y - r), r)
+        else:
+            circle(screen, color['white'], (x + i, y), r)
+
+
+def draw_house(x, y, wedth, height):
+    """
+    функция рисует домик с окном и треугольной крышей в средине
+    :param x: начальная координата x (где рисовать)
+    :param y: начальная координата y (где рисовать)
+    :param wedth: ширина домика
+    :param height: высота домика
+    :return:
+    """
+    rect(screen, color['brown'], (x, y, wedth, height))  # корпус (стеныб каркас)
+    polygon(screen, color['pink'], [(x, y), (x+wedth/2, wedth), (x+wedth, y)])  # крыша
+    rect(screen, color['blue'], (x + wedth/4, y + height/3, wedth/2, height/3))  # (окно)
 
 
 draw_cloud(100, 90, 20)
@@ -49,8 +56,10 @@ draw_cloud(400, 135, 15)
 draw_cloud(650, 100, 20)
 # нарисовал солнце
 circle(screen, color['yellow'], (35, 35), 25)
-# рисуем дом
-
+# рисуем дом 1
+draw_house(70, 350, 200, 180)
+# рисуем дом 2
+draw_house(500, 250, 140, 120)
 
 pygame.display.update()
 clock = pygame.time.Clock()
